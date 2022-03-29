b29e5efe072ecf0206f325bb8f6d9ab69872b284
Manish Girdhar <manish.girdhar@gmail.com>
2022-03-30 10:08:03 +1100

    feat: variables

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
-package main
+package main   //first line must be package name and file name must end with  .go
+
+import "fmt"
 
 func main() {
+value1,value2:=returntwostrings(); //assigning values from function retruning two values
+fmt.Println(value2,value1);
+}
+
+func first() string{
+	//declared variable must be used otherwise go will not compile
+	fmt.Println("Hello! ");
+	var  myname string;       //string variable
+	myname="Manish";          //assigning value to variable
+	var age int;             //int variable defaults to int64 if machine is  64 bit
+	age=42;                  //assign int value
+	lastname := "Girdhar"   //string without variable declaration
+	fmt.Println(myname,lastname,age);
+	return "done"
+}
+
+func returntwostrings() (string,string){ //in go function can retrun two values
+valuefromfirst:=first() ;
+return valuefromfirst, "secondstring"
 }
